06321a7dd06bdd740af8550a46e13cc3a34a37c9
jdr0driguez <jrodriguez@liquitty.com>
2025-05-21 15:36:54 -0500

    Update browser_config.py

--- browser_config.py
+++ browser_config.py
@@ -1,15 +1,15 @@
-# browser_config.py
+
 
 import os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 def configurar_driver():
-    # Configurar carpeta de descargas
+    
     download_dir = os.path.abspath("downloads")
     os.makedirs(download_dir, exist_ok=True)
 
-    # Opciones de Chrome
+    
     options = Options()
     prefs = {
         "download.default_directory": download_dir,
