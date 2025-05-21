edf327f939f335e326f032dd2c74d0d55409afd3
jdr0driguez <jrodriguez@liquitty.com>
2025-05-21 15:32:42 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,23 +6,23 @@ from browser_config import configurar_driver
 from sendAPI import enviar_archivo_api
 
 def run():
-    #  Iniciar navegador con configuración centralizada
+    
     driver = configurar_driver()
 
     try:
-        # Login con Selenium
+        
         login_selenium(driver, EMAIL, PASSWORD, BASE_URL)
 
-        # Obtenemos la ruta actual de documentos
+        
         obtener_ruta_documentos()
 
-        # Descargar CSV
+        
         download_csv_selenium(driver)
 
         enviar_archivo_api()
 
     finally:
-        # Cerrar navegador
+        
         driver.quit()
 
 if __name__ == "__main__":
